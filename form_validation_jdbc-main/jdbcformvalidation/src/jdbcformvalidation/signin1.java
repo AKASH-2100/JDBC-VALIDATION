6b3ae41f92d3d910a30d03d0ee6cdd54ab165198
AKASH-2100 <149417326+AKASH-2100@users.noreply.github.com>
2024-08-03 15:36:13 +0530

    Update signin1.java

--- form_validation_jdbc-main/jdbcformvalidation/src/jdbcformvalidation/signin1.java
+++ form_validation_jdbc-main/jdbcformvalidation/src/jdbcformvalidation/signin1.java
@@ -14,7 +14,7 @@ public class signin1 {
 	}
 	void store() throws SQLException, ClassNotFoundException {
 		Connection con = dbconnection.dbconnections();
-		String query = "insert into datass(userid,pass) values(?,?)";
+		String query = "insert into login(userid,pass) values(?,?)";
 		PreparedStatement st = con.prepareStatement(query);
 		st.setString(1, username);
 		st.setString(2, password); 
