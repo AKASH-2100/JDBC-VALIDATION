4a642ddbf0885b7d21f480bf2a670a946405f18b
AKASH-2100 <149417326+AKASH-2100@users.noreply.github.com>
2024-08-03 15:33:18 +0530

    Update dbconnection.java

--- form_validation_jdbc-main/jdbcformvalidation/src/jdbcformvalidation/dbconnection.java
+++ form_validation_jdbc-main/jdbcformvalidation/src/jdbcformvalidation/dbconnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class dbconnection {
 	//Class.forName("com.mysql.cj.jdbc.Driver");
-	private static final String url = "jdbc:mysql://localhost:3306/form";
+	private static final String url = "jdbc:mysql://localhost:3306/demo";
 	private static final String user = "root";
 	private static final String pass = "root";
 	public static Connection dbconnections() throws SQLException {
